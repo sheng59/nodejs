67723b9a6e46f60d1ef72007cf06e9646a73872e
A-Gui <sa6549872000@gmail.com>
2026-02-10 17:00:45 +0800

    Refactor server.js to include CORS and Supabase
    Added CORS support and integrated Supabase client.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors')
 const axios = require('axios');
+const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
 
@@ -9,10 +11,16 @@ const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
 const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN;
 
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	res.send('LINE Bot Server on Vercel 🚀');
+	res.send('Shopping Backend API');
 });
 
 module.exports = app;
